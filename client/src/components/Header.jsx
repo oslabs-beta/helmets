fe2d6128073f05ddb43eb0f9107d25be3d6aae3c
Ry G <Unstoppable@Unstoppable.local>
2023-05-10 12:28:28 -0700

    converting file data from input file object to a webform for upload. *working*

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
  
+
+//the header provides a space for the Helmets logo and an input field that allows the user to select a chart folder to upload
+//the selected folder is then passed to the database
+//the backend will return an object that is then used to populate the <Flow /> component
 const Header = () => {
   const [buttonText, setButtonText] = useState('Select Chart');
 
-  //helper function to clear a passed-in list element
+  //helper function to clear the unordered list in the header and reset the input target to nothing
   const resetHeader = (listEl) => {
     const inputTarget = document.getElementById('chartPicker');
     if(listEl.childElementCount > 0) {
@@ -14,23 +18,61 @@ const Header = () => {
     };
     return;
   }
+  //declare a let to hold the files selected by user during handleChange()
+  let selectedFiles;
 
   /* The handleChange() function to allow user to select a folder. contents are displayed.*/
   /*TO DO >> post contents to database */
-  const handleChange = (event) => {
-    const fileInfo = document.getElementById('fileInfo');
-
-    if(fileInfo.childElementCount > 0) {
-      while(fileInfo.firstChild) {
-        fileInfo.removeChild(fileInfo.firstChild);
+  const handleChange = async (event) => {
+    let chartFormData;
+    const fileCache = event.target.files;
+    chartFormData = new FormData();
+    //manages display of selected data on the front-end
+    try{
+      //update selectedFiles with the event data for use outside of handleChange()
+      selectedFiles = event.target;
+      //grab the html element of the unordered list
+      const fileInfo = document.getElementById('fileInfo');
+      //if there is already content in the unordered list, clear it out
+      if(fileInfo.childElementCount > 0) {
+        while(fileInfo.firstChild) {
+          fileInfo.removeChild(fileInfo.firstChild);
+        }
       }
+      //iterate over it to populate the unordered list
+      for(const file of fileCache) {
+        const item = document.createElement('li');
+        item.textContent = 'File Name: ' + file.name + '\nRelative Path: ' + file.webkitRelativePath;
+        fileInfo.appendChild(item);
+      }
+    } catch (error) { 
+      console.log('error occurred during front-end operations while managing selected chart:', error);
     }
-
-    for(const file of event.target.files) {
-      const item = document.createElement('li');
-      item.textContent = 'File Name: ' + file.name + '\nRelative Path: ' + file.webkitRelativePath;
-      fileInfo.appendChild(item);
+    //convert the fileList to a new FormData in preparation for uploading to database
+    try{
+      for(const file of fileCache) {
+        chartFormData.append('files', file, file.name);
+      }
+      for(var pair of chartFormData.entries()) {
+        console.log(pair[0]+', '+pair[1].name+' Path = '+pair[1].webkitRelativePath);
+      }
+    } catch (error) {
+      console.log('error occurred while converting fileList to FormData: ', error);
     }
+    //upload FormData to database
+    // try{
+    //   const options = {
+    //     method: 'POST',
+    //     body: formData,
+    //     headers: {
+    //       'Content-Type': 'multipart/form-data'
+    //     }
+    //   };
+      
+    //   fetch('/upload', options)   // <<< UPDATE POST LOCATION URL
+    // } catch (error) {
+    //   console.log('error occurred while attempting to upload FormData to database:', error);
+    // }
   }
  
   return (
