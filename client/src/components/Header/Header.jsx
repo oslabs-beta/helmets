257418a6d2e39cd9f3493b803d8215306a79037f
Lane Hamilton <aleyna.hamilton@gmail.com>
2023-05-31 20:28:05 -0700

    main page layout with about button

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -13,50 +13,52 @@ const Header = ({ handleChange, submitChart, disabled, resetHeader }) => {
       <button className="show-modal" onClick={() => setShow(true)}>
         About
       </button>
-      <Modal show={show} onClose={() => setShow(false)} />
-      <div className="img-container">
-        <img
-          src={logo}
-          alt="logo image of three spartan helmets with the text helmets underneath"
-        />
-      </div>
-      <div className="input-container">
-        <div className="file-display">
-          <div className="list-display">
-            <ul title="Relative Path" id="fileInfo"></ul>
-          </div>
-          <form
-            encType="multipart/form-data"
-            method="post"
-            className="input-form"
-            id="chartPicker"
-          >
-            <input
-              className="custom-file-input"
-              type="file"
-              name="uploaded_file"
-              onChange={handleChange}
-              directory=""
-              webkitdirectory=""
-              mozdirectory=""
-              // disabled={disabled}
-            />
-          </form>
-          <button
-            onClick={() => resetHeader()}
-            id="clearBtn"
-            disabled={disabled}
-          >
-            Clear Selection
-          </button>
+      <div className="flex-container">
+        <Modal show={show} onClose={() => setShow(false)} />
+        <div className="img-container">
+          <img
+            src={logo}
+            alt="logo image of three spartan helmets with the text helmets underneath"
+          />
+        </div>
+        <div className="input-container">
+          <div className="file-display">
+            <div className="list-display">
+              <ul title="Relative Path" id="fileInfo"></ul>
+            </div>
+            <form
+              encType="multipart/form-data"
+              method="post"
+              className="input-form"
+              id="chartPicker"
+            >
+              <input
+                className="custom-file-input"
+                type="file"
+                name="uploaded_file"
+                onChange={handleChange}
+                directory=""
+                webkitdirectory=""
+                mozdirectory=""
+                // disabled={disabled}
+              />
+            </form>
+            <button
+              onClick={() => resetHeader()}
+              id="clearBtn"
+              disabled={disabled}
+            >
+              Clear Selection
+            </button>
 
-          <button
-            id="submitBtn"
-            onClick={() => submitChart()}
-            disabled={disabled}
-          >
-            Submit Chart
-          </button>
+            <button
+              id="submitBtn"
+              onClick={() => submitChart()}
+              disabled={disabled}
+            >
+              Submit Chart
+            </button>
+          </div>
         </div>
       </div>
     </header>
